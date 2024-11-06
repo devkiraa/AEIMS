e7564557cbadd1b0b28b7df610099315a43cf806
NarayananG12 <narayanangs763@gmail.com>
2024-11-06 18:34:38 +0530

    after-event-insert

--- db/triggers/insert-venue-books.sql
+++ db/triggers/insert-venue-books.sql
@@ -1,12 +1,31 @@
 DELIMITER //
 
-CREATE TRIGGER insert_venue_books
+CREATE TRIGGER after_event_insert
 AFTER INSERT ON event_tb
 FOR EACH ROW
 BEGIN
-    INSERT INTO venues_bookings (ven_book_id, ven_id, start_date, end_date, start_time, end_time, ven_stat)
-    VALUES (NEW.evn_id, NEW.ven_id, NEW.event_sd, NEW.evn_ed, NEW.evn_st, NEW.event_et, NEW.evn_stat);
+    INSERT INTO venues_bookings (
+        ven_id,
+        start_date,
+        end_date,
+        start_time,
+        end_time,
+        ven_stat,
+        evn_id,
+        quick_evn
+    )
+    VALUES (
+		NEW.ven_id,
+        NEW.event_sd,
+        NEW.evn_ed,
+        NEW.evn_st,
+        NEW.event_et,
+        1,
+        NEW.evn_id,
+        NULL
+    );
 END //
 
 DELIMITER ;
-    
+
+
