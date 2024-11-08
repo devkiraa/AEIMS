66f3d65415d4fd07437e1b4a01af6e72d7431599
devkiraa <devkiraa@gmail.com>
2024-11-08 12:07:55 +0530

    updated node

--- models/db.js
+++ models/db.js
@@ -1,16 +1,25 @@
-// db.js
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
+(async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Database connected successfully");
+    connection.release(); // Release the connection back to the pool
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+  }
+})();
 
 module.exports = pool;
