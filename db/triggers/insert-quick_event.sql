78f4571eb48b30fff3786bba7b8a627fe6baf812
NarayananG12 <narayanangs763@gmail.com>
2024-11-12 19:45:50 +0530

    insert-quick_event

---
+++ db/triggers/insert-quick_event.sql
@@ -0,0 +1,25 @@
+SELECT * FROM aeims.event_quick;
+
+DELIMITER //
+
+CREATE TRIGGER insert_into_vnb_from_qck
+AFTER INSERT ON event_quick
+FOR EACH ROW
+BEGIN
+    INSERT INTO venues_bookings 
+    (ven_id, start_date, end_date, start_time, end_time, ven_stat, qck_evn_id)
+    VALUES (
+        NEW.ven_id,
+        NEW.evn_qk_sd,
+        NEW.evn_qk_ed,
+        NEW.evn_qk_st,
+        NEW.evn_qk_et,
+        CASE
+            WHEN NEW.qck_stat = 1 THEN 1
+            ELSE 0
+        END,
+        NEW.evn_qk_id  -- assuming you want to use the evn_qk_id here
+    );
+END //
+
+DELIMITER ;
